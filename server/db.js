e06c9875607effe1853d94aef7da65b9501ff11f
Thalia Montreux <88428594+thaliamontreux@users.noreply.github.com>
2026-04-20 20:11:18 -0500

    Fix bcrypt import in db initialization

--- server/db.js
+++ server/db.js
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise';
 import fs from 'node:fs/promises';
 import crypto from 'node:crypto';
 import path from 'node:path';
+import bcrypt from 'bcryptjs';
 
 let pool = null;
 
