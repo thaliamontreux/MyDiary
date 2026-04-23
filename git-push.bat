f1b6be420bcc1341b8d4dba659d62879c9aaffe7
Thalia Montreux <88428594+thaliamontreux@users.noreply.github.com>
2026-04-23 00:18:12 -0500

    Thu 04/23/2026  0:18:12.00

---
+++ git-push.bat
@@ -0,0 +1,19 @@
+@echo off
+echo === MyDiary Git Push Script ===
+cd /d f:\project\DiaryApp\CascadeProjects\windsurf-project
+
+echo.
+echo [1/3] Adding changes...
+git add -A --verbose
+
+echo.
+echo [2/3] Committing: %date% %time%
+git commit -m "%date% %time%"
+
+echo.
+echo [3/3] Pushing to GitHub...
+git push
+
+echo.
+echo === Done! ===
+pause
